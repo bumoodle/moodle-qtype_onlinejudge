580937d8f2105da2a8cc929619dbb1f49727706f
Kyle Temkin <ktemkin@binghamton.edu>
2012-07-31 15:23:38 -0400

    BU-1 fixed an error in which testbench files were not properly copied to a draft area for editing

--- edit_vhdl_form.php
+++ edit_vhdl_form.php
@@ -39,6 +39,9 @@ require_once($CFG->dirroot.'/question/type/edit_question_form.php');
  */
 class qtype_vhdl_edit_form extends question_edit_form 
 {
+    /**
+     * A set of default options for the testbench upload form.
+     */
 	private static $hdl_file_options = 
 			array(
         	'maxfiles' => 25,
@@ -84,56 +87,60 @@ class qtype_vhdl_edit_form extends question_edit_form
         
     }
 
-    /*
+    /**
+     * Preprocess the form data, creating any necessary file areas. 
+     *
+     * @param question_vhdl_question    The core question definition to be modified.
+     * @return question_vhdl_question   The question, which was modified by preprocessing.
+     */
     protected function data_preprocessing($question) 
     {
+        //perform the base modifications to the question type
         $question = parent::data_preprocessing($question);
+        
+        //if we have an existing file itemID, use it to import the existing files
+        $itemid = empty($question->options->testbench) ? null : $question->options->testbench;
 
-        if (empty($question->options)) {
-            return $question;
-        }
+        //if a draft area exists for the current form, get its ID
+        $draftitemid = file_get_submitted_draft_itemid('testbench');
 
+        //Prepare a draft area for the testbench file upload.
+        //If we didn't get a valid draft area in the last step, then one will be automatically created.
+        //If we _did_ get a valid file handle (itemid), permanent files already exist. Copy them to the draft area for modification.
+        file_prepare_draft_area($draftitemid, $this->context->id, 'qtype_vhdl', 'testbench', $itemid, self::$hdl_file_options);
 
-    	//return array('question_vhdl', 'hdltype', 'allowmulti', 'testbench', 'autofeedback');
+        //Replace the question's testbench with a reference to the draft file area.
+        //If a testbench previously existed, copy it there.
+        $question->testbench = $draftitemid;
 
-        $question->hdltype = $question->options->hdltype;
-        $question->allowmulti = $question->options->hdltype;
-        $question->autofeedback = $question->options->autofeedback;
-
-        $draftid = file_get_submitted_draft_itemid('testbench');
-
-
-        $question->testbench = array();
-        $question->testbench['text'] = file_prepare_draft_area(
-            $draftid,           // draftid
-            $this->context->id, // context
-            'qtype_vhdl',      // component
-            'testbench',       // filarea
-            !empty($question->id) ? (int) $question->id : null, // itemid
-            $this->fileoptions, // options
-            $question->options->testbench // text
-        );
-        $question->testbench['itemid'] = $draftid;
-
+        //return the newly updated question
         return $question;
     }
 
+    /**
+     * Performs basic form validation, ensuring that the input is valid.
+     * 
+     * @param $fromform     The partially santized post-data submitted by the user.
+     * @param $file         An array of uploaded user-files, before they are saved to the draft area.
+     *
+     * @returns array       An array of errors which occurred during validation. If no errors occur, returns an empty array.
      */
- 
-
-
     function validation($fromform, $files)
     {
-    		
-			$errors = parent::validation($fromform, $files);
-			
-			if(!isset($fromform['testbench']))
-				$errors['testbench'] = get_string('notestbench', 'qtype_vhdl');
-			
-    		return $errors;
-    	
+        //perform the main validation
+        $errors = parent::validation($fromform, $files);
+       
+        //if the user did not include a testbench, then throw an error
+        if(!isset($fromform['testbench']))
+            $errors['testbench'] = get_string('notestbench', 'qtype_vhdl');
+		
+        //return the completed list of errors
+        return $errors;
     }
-    
+   
+    /**
+     * Returns the question type suffix, for identification.
+     */
     function qtype() 
     {
         return 'vhdl';
